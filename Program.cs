219937cc03b9d174120158152868a6261af6ef3a
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2024-12-17 23:32:05 +0530

    [Pooja] added: Main method

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SinglyLinkedList slt = new SinglyLinkedList();
+            //slt.AddNode(28);
+            //slt.AddNode(30);
+            //slt.AddNode(42);
+            //slt.AddNode(15);
+            //slt.AddNode(17);
+            //slt.AddNode(28);
+
+            //Console.WriteLine(slt.PrintList());
+            //slt.AddFront(10);
+            //Console.WriteLine(slt.PrintList());
+            //slt.DeleteLast();
+            //Console.WriteLine(slt.PrintList());
+            //slt.DeleteFirst();
+            //Console.WriteLine(slt.PrintList());
+
+            //Stack stk = new Stack();
+            //stk.Push(10);
+            //stk.Push(15);
+            //stk.Push(20);
+            //stk.Push(30);
+            //stk.Push(35);
+
+            //Console.WriteLine(stk.PrintStack());
+            //stk.Pop();
+            //stk.Pop();
+            //Console.WriteLine(stk.PrintStack());
+
+            //Queue q = new Queue();
+            //q.Enqueue(5);
+            //q.Enqueue(6);
+            //q.Enqueue(7);
+            //q.Enqueue(8);
+            //q.Enqueue(9);
+            //q.Enqueue(10);
+
+            //Console.WriteLine(q.PrintQueue());
+            //q.Dequeue();
+            //Console.WriteLine(q.PrintQueue());
+        }
+    }
+}
